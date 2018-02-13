88190eb81ea9cbf9dbc2a3eda36c0955a5042dda
Sam M <samstevenm@gmail.com>
2018-02-13 02:47:34 -0800

    Update vivesmall.sh

--- vivesmall.sh
+++ vivesmall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -eu
 ip_addr="$(nmap -n -sn 192.168.2.1/24 -oG - | awk '/Up$/{print $2}' | tail -n+2)"
-pre='URL=https://'
-url=$pre$ip_addr
+pre='https://'
+url="$pre$ip_addr"
 script='Exec=/home/joel/vivesmall.sh'
 echo '[Desktop Entry]' > ~/Desktop/ViveSmall.desktop
 echo 'Encoding=UTF-8' >> ~/Desktop/ViveSmall.desktop
@@ -10,5 +10,5 @@ echo 'Type=Application' >> ~/Desktop/ViveSmall.desktop
 echo 'Terminal=False' >> ~/Desktop/ViveSmall.desktop
 echo $script >> ~/Desktop/ViveSmall.desktop
 echo 'Icon=text-html' >> ~/Desktop/ViveSmall.desktop
-chromium-browser --password-store=basic --chrome-frame --window-size=425,692 --window-position=0,80 --app="($url)"
+chromium-browser --password-store=basic --chrome-frame --window-size=425,692 --window-position=0,80 --app=$url
 #chromium-browser --password-store=basic --kiosk --chrome-frame --app="($url)"
