fd5be5a34d8e50d0caea80fd4771924c13e75d46
Sam Myers <sam@themyers.party>
2021-09-26 13:40:02 -0700

    sync all files

--- changeip.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh -eu
-nmcli connection modify 'Wired connection 1' +ipv4.addresses 192.168.2.1/24
-sudo ip addr flush 'enxb827eb76559a'
-sudo systemctl restart networking
--- startvncviewer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh -eu
-sudo systemctl start vncserver-x11-serviced.service
-sudo systemctl enable vncserver-x11-serviced.service
